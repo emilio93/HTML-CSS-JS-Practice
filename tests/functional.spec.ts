6e3dbe811281ed4dc88a31d991ca5c71cd1db4c1
Emilio Rojas <emilio.rojas@ucr.ac.cr>
2021-07-28 01:42:44 -0600

    Actually do some tests

--- tests/functional.spec.ts
+++ tests/functional.spec.ts
@@ -1,54 +1,74 @@
-import { test, expect } from '@playwright/test';
-import * as path from 'path';
+import { test, expect } from "@playwright/test";
+import * as path from "path";
 
 const URL = `file:///${path.dirname(__dirname)}/src/index.html`;
 const CHECKBOX_QTY = 4;
-const ROWS_TO_INSERT = 20;
+const ROWS_TO_INSERT = 30;
 
-const emails = ['juan@gmail.com', 'pedro@gmail.com', 'luis@gmail.com', 'gerardo@gmail.com', 'miguel@gmail.com'];
-const names = ['juan', 'miguel','luisa', 'daniela', 'pablo', 'oscar'];
+const emails = [
+  "juan@gmail.com",
+  "pedro@gmail.com",
+  "luis@gmail.com",
+  "gerardo@gmail.com",
+  "miguel@gmail.com",
+];
+const names = ["juan", "miguel", "luisa", "daniela", "pablo", "oscar"];
+
+const randomBool = () => (Math.round(Math.random()) === 0 ? false : true);
 
 const submitRandomValues = async (page) => {
   const randomValues = {
-    email: emails[Math.floor(Math.random()*emails.length)],
-    name: names[Math.floor(Math.random()*names.length)],
-    country: Math.floor(Math.random()*200),
-    gender: Math.floor(Math.random()*3),
+    email: randomBool() ? emails[Math.floor(Math.random() * emails.length)] : "",
+    name: randomBool() ? names[Math.floor(Math.random() * names.length)] : "",
+    country: randomBool() ? Math.floor(Math.random() * 200) : "",
+    gender: randomBool() ? Math.floor(Math.random() * 3) : "",
     hobby: [],
-    date: {
-      day:Math.ceil(Math.random()*28),
-      month:Math.ceil(Math.random()*12),
-      year:Math.ceil(Math.random()*(2020-1980)+1980),
-    }
+    date: randomBool() ? {
+      day: Math.ceil(Math.random() * 28),
+      month: Math.ceil(Math.random() * 12),
+      year: Math.ceil(Math.random() * (2020 - 1980) + 1980),
+    } : null,
   };
 
-  if (Math.round(Math.random())) await page.fill('#email', randomValues.email);
-  if (Math.round(Math.random())) await page.fill('#name', randomValues.name);
-  if (Math.round(Math.random())){
-    const date = randomValues.date;
-    const dateStr = `${date.year}-${date.month<10?'0':''}${date.month}-${date.day<10?'0':''}${date.day}`
-    await page.fill('#birthdate', dateStr);
+  await page.fill("#email", randomValues.email);
+  await page.fill("#name", randomValues.name);
+
+  const date = randomValues.date;
+  if (date !== null) {
+    const dateStr = `${date.year}-${date.month < 10 ? "0" : ""}${date.month}-${date.day < 10 ? "0" : ""}${date.day}`;
+    await page.fill("#birthdate", dateStr);
   }
-  if (Math.round(Math.random())) await page.selectOption('#country', `${randomValues.country}`);
-  if (Math.round(Math.random())) await page.check(`#gender${randomValues.gender}`);
-  if (Math.round(Math.random())) {
-    for (let index = 0; index < CHECKBOX_QTY; index++) {
-      if (Math.round(Math.random())) {
-        await page.check(`#hobby${index}`);
-        randomValues.hobby.push(true);
-      } else {
-        randomValues.hobby.push(false);
-      }
+
+  await page.selectOption("#country", `${randomValues.country}`);
+  if (randomValues.gender !== "") await page.check(`#gender${randomValues.gender}`);
+  for (let index = 0; index < CHECKBOX_QTY; index++) {
+    if (randomBool()) {
+      await page.check(`#hobby${index}`);
+      randomValues.hobby.push(true);
+    } else {
+      randomValues.hobby.push(false);
     }
   }
-  await page.click('#registerButton');
+  await page.click("#registerButton");
   return randomValues;
 };
 
-test('basic test', async ({ page }) => {
+test("basic test", async ({ page }) => {
   await page.goto(URL);
+  const randomValues = [];
   for (let index = 0; index < ROWS_TO_INSERT; index++) {
-    await submitRandomValues(page);
+    const randomValuesRow = await submitRandomValues(page);
+    randomValues.push(randomValuesRow);
+  }
+  const rows = await page.$$(".user-table-row");
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    const cells = await row.$$(".user-table-row-cell");
+
+    ["email", "name"].forEach(async (key, index) => {
+      const html = await cells[index].innerText();
+      expect(html).toEqual(randomValues[i][key]);
+    });
   }
   await page.screenshot({ path: `screenshot.png`, fullPage: true });
 });
